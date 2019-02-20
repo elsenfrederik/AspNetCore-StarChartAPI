40f33fad3c116173714a6631bf57a8e1f21af9e6
Frederik.Elsen <frederik.elsen@gfk.com>
2019-02-20 22:30:14 +0100

    Completed the module

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -8,7 +8,6 @@ namespace StarChart.Data
         public DbSet<CelestialObject> CelestialObjects { get; set; }
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
-            
         }
     }
 }
